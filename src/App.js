915a03150e042154bddfcfad11233b3ed66b2596
Taylor Bancroft <tebancro@gmail.com>
2021-03-08 16:53:47 -0600

    Project Setup

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component} from 'react';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+class App extends Component{
+  render() {
+  return(
+    <div className="App">
+      <h2>Portfolio Project</h2>
+    </div>
+  );
+  }
+}
+export default App;
